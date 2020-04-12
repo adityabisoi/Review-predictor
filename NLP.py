0aa5cb4edca2b827ecf9f8827feb5a502b126a28
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-04-12 14:47:07 +0530

    Text cleaning for all reviews

--- NLP.py
+++ NLP.py
@@ -5,17 +5,20 @@ import pandas as pd
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)   #Default value to ignore quotes is 3
 
 # Cleaning texts
-import re
-review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
-review = review.lower()
-
 # Removing non-determiners and stemming
+import re
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-review = review.split()
-words = stopwords.words('english')
 ps = PorterStemmer()
-review = [ps.stem(i) for i in review if i not in set(words)]    # set() is optional, can be used for faster filtering
-review = ' '.join(review)
+words = stopwords.words('english')
+corpus = []                                                        # Initialize an empty list for all reviews
+
+for x in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][x])
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(i) for i in review if i not in set(words)]    # set() is optional, can be used for faster filtering
+    review = ' '.join(review)
+    corpus.append(review)
